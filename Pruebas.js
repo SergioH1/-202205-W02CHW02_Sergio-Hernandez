96bc5a0baedaa3acba31d8657345b918669ce346
Sergio H <sergiohernandezroman@gmail.com>
2022-05-22 15:03:14 +0200

    add upgrade app.js & ramdonCellBoard.js

--- Pruebas.js
+++ Pruebas.js
@@ -1,10 +1,137 @@
-function boardGame(f, c) {
-  let obj = new Array(f);
-  for (let y = 0; y < f; y++) {
-    obj[y] = new Array(c);
+// const cellBoard = [
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 0, 0, 0],
+// ];
+// const cellBoardClone = cellBoard.map((cellBoard) => [...cellBoard]);
+
+// let livingCells = 0;
+// export function gameOfLive(array, array2) {
+//   for (let x = 0; x < cellBoard.length; x++) {
+//     for (let y = 0; y < cellBoard.length; y++) {
+//       if (x === 0 && y === 0) {
+//         if (array[x][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y] === 1) {
+//           livingCells++;
+//         }
+//       } else if (x === 0 && y < array.length && y !== 0) {
+//         if (array[x][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y + 1] === 1) {
+//           livingCells++;
+//         }
+//       } else if (x === 0 && y === array[x].length) {
+//         if (array[x][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y - 1] === 1) {
+//           livingCells++;
+//         }
+//       } else if (x === array.length - 1 && y === 0) {
+//         if (array[x][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y] === 1) {
+//           livingCells++;
+//         }
+//       } else if (x === array.length - 1 && y < array[x].length && y !== 0) {
+//         if (array[x][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y + 1] === 1) {
+//           livingCells++;
+//         }
+//       } else if (x === array.length && y === array[x].length) {
+//         if (array[x][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y - 1] === 1) {
+//           livingCells++;
+//         }
+//       } else {
+//         if (array[x][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y - 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x - 1][y + 1] === 1) {
+//           livingCells++;
+//         }
+//         if (array[x + 1][y - 1] === 1) {
+//           livingCells++;
+//         }
+//       }
+//       if (livingCells < 2 || livingCells > 3) {
+//         array2[x][y] = 0;
+//       }
+//       if ((livingCells === 2 && livingCells === 3) || livingCells === 3) {
+//         array2[x][y] = 1;
+//       }
+//       livingCells = 0;
+//     }
+//   }
+//   return array2;
+// }
+
+// console.log(cellBoard);
+// console.log(gameOfLive(cellBoard, cellBoardClone));
+
+function RamdonArray(rows, cols) {
+  const board = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    board[i] = new Array(cols);
+    for (let j = 0; j < cols; j++) {
+      board[i][j] = Math.round(Math.random());
+    }
   }
-
-  return obj;
+  return board;
 }
-
-console.log(boardGame(10, 10));
+console.log(RamdonArray(5, 5));
--- app.js
+++ app.js
@@ -1,184 +1,105 @@
-const cellBoard = [
-  [0, 0, 0, 0, 0],
-  [0, 1, 1, 0, 0],
-  [0, 0, 1, 1, 0],
-  [0, 1, 1, 0, 0],
-  [0, 0, 0, 0, 0],
-];
+// const cellBoard = [
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 0, 0, 0],
+// ];
 
-let livingCells = 0;
-
-for (let x = 0; x < cellBoard.length; x++) {
-  for (let y = 0; y < cellBoard[x].length; j++) {
-    if (x === 0 && y === 0) {
-      if (cellBoard[x + 1][y + 1] === 1) {
-        livingCells++;
-      }
-      if (cellBoard[x][y + 1] === 1) {
-        livingCells++;
-      }
-      if (cellBoard[x + 1][y] === 1) {
-        livingCells++;
-      }
-    } else {
-      if (x === cellBoard.length && y === cellBoard.length) {
-        if (cellBoard[x - 1][y - 1] === 1) {
+export function gameOfLive(array) {
+  const cellBoardClone = array.map((array) => [...array]);
+  let livingCells = 0;
+  for (let x = 0; x < cellBoardClone.length; x++) {
+    for (let y = 0; y < cellBoardClone.length; y++) {
+      if (x === 0 && y === 0) {
+        if (array[x][y + 1] === 1) {
           livingCells++;
         }
-        if (cellBoard[x][y - 1] === 1) {
+        if (array[x + 1][y + 1] === 1) {
           livingCells++;
         }
-        if (cellBoard[x - 1][y] === 1) {
+        if (array[x + 1][y] === 1) {
+          livingCells++;
+        }
+      } else if (x === 0 && y < array.length && y !== 0) {
+        if (array[x][y - 1] === 1) {
+          livingCells++;
+        }
+        if (array[x][y + 1] === 1) {
+          livingCells++;
+        }
+        if (array[x + 1][y] === 1) {
+          livingCells++;
+        }
+        if (array[x + 1][y - 1] === 1) {
+          livingCells++;
+        }
+        if (array[x + 1][y + 1] === 1) {
+          livingCells++;
+        }
+      } else if (x === array.length - 1 && y === 0) {
+        if (array[x][y + 1] === 1) {
+          livingCells++;
+        }
+        if (array[x - 1][y + 1] === 1) {
+          livingCells++;
+        }
+        if (array[x - 1][y] === 1) {
+          livingCells++;
+        }
+      } else if (x === array.length - 1 && y < array[x].length && y !== 0) {
+        if (array[x][y - 1] === 1) {
+          livingCells++;
+        }
+        if (array[x][y + 1] === 1) {
+          livingCells++;
+        }
+        if (array[x - 1][y] === 1) {
+          livingCells++;
+        }
+        if (array[x - 1][y - 1] === 1) {
+          livingCells++;
+        }
+        if (array[x - 1][y + 1] === 1) {
           livingCells++;
         }
       } else {
-        if (x === 0 && y === cellBoard.length) {
-          if (cellBoard[x + 1][y] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x + 1][y - 1] === 1) {
-            livingCells++;
-          }
-        }
-        if (x === cellBoard.length && y === 0) {
-          if (cellBoard[x - 1][y + 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y + 1] === 1) {
-            livingCells++;
-          }
-        }
-
-        if (y === cellBoard.length) {
-          if (cellBoard[x + 1][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x + 1][y] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y - 1] === 1) {
-            livingCells++;
-          }
-        }
-
-        if (x === 0) {
-          if (cellBoard[x + 1][y + 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x + 1][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y + 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x + 1][y] === 1) {
-            livingCells++;
-          }
-        }
-        if (y === 0) {
-          if (cellBoard[x + 1][y + 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x + 1][y] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y + 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y + 1] === 1) {
-            livingCells++;
-          }
-        }
-
-        if (x === cellBoard.length) {
-          if (cellBoard[x - 1][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y + 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y + 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y - 1] === 1) {
-            livingCells++;
-          }
-        }
-
-        if (x === cellBoard.length && y === cellBoard.length) {
-          if (cellBoard[x - 1][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x][y - 1] === 1) {
-            livingCells++;
-          }
-          if (cellBoard[x - 1][y] === 1) {
-            livingCells++;
-          }
-        }
-        if (x === 0 && y === cellBoard.length) {
-        }
-        if (y === 0 && cellBoard.length) {
-        }
-
-        if (cellBoard[x + 1][y + 1] === 1) {
+        if (array[x][y - 1] === 1) {
           livingCells++;
         }
-        if (cellBoard[x - 1][y - 1] === 1) {
+        if (array[x][y + 1] === 1) {
           livingCells++;
         }
-        if (cellBoard[x - 1][y + 1] === 1) {
+        if (array[x - 1][y] === 1) {
           livingCells++;
         }
-        if (cellBoard[x + 1][y - 1] === 1) {
+        if (array[x + 1][y] === 1) {
           livingCells++;
         }
-        if (cellBoard[x][y + 1] === 1) {
+        if (array[x - 1][y - 1] === 1) {
           livingCells++;
         }
-        if (cellBoard[x][y - 1] === 1) {
+        if (array[x + 1][y + 1] === 1) {
           livingCells++;
         }
-        if (cellBoard[x + 1][y] === 1) {
+        if (array[x - 1][y + 1] === 1) {
           livingCells++;
         }
-        if (cellBoard[x - 1][y] === 1) {
+        if (array[x + 1][y - 1] === 1) {
           livingCells++;
         }
       }
+      if (livingCells < 2 || livingCells > 3) {
+        cellBoardClone[x][y] = 0;
+      }
+      if ((livingCells === 2 && livingCells === 3) || livingCells === 3) {
+        cellBoardClone[x][y] = 1;
+      }
+      livingCells = 0;
     }
   }
+  return cellBoardClone;
 }
 
-// celula vive o muere
-
-//
-// if ((cellBoard[x] === 0 && cellBoard[y] === 0) || livingCells === 3) {
-// }
-
-// if(livingCells === 2 || livingCells === 3){
-
-// }
-
-// if(livingCells > 3 ) // muere
+// console.log(cellBoard);
+// console.log(gameOfLive(cellBoard));
